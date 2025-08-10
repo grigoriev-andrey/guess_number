e76139e46092623a8bbe1a803e7548e86fea3762
Andrey Grigoriev <andreygrigoriev3112@gmail.com>
2025-08-10 16:18:07 +1000

    te

--- guess_number.py
+++ guess_number.py
@@ -1,22 +1,22 @@
 from random import randint
 
-
 number_1 = randint(1, 100)
 
 
 def main():
     while True:
-        guess = int(input('Your number: '))
+        guess = int(input("Your number: "))
         if number_1 > guess:
-            print('your number is smaller than guess')
+            print("your number is smaller than guess")
 
         elif number_1 < guess:
-            print('your number is bigger than guess')
+            print("your number is bigger than guess")
         elif guess == number_1:
             break
 
 
 main()
 
-print('Good game, men')
-print('Good game, women')
+
+print("Good game, men")
+print("Good game, women")
